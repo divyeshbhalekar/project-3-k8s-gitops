2a2b3600ee220c7175fa64848aa7a4938f761f7b
Divyesh Bhalekar <89533742+divyeshbhalekar@users.noreply.github.com>
2024-03-20 17:18:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from urllib.parse import quote
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Thankyou for checking my repo TY!!!'
+    return 'Thank you for checking my repo TY!!!'
