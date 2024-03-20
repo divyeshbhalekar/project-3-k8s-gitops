e3f8bb2b31c82ad62df609c9df2040ab29d6ba6d
Divyesh3011 <divyesh@microsec.ai>
2024-03-20 18:25:31 +0530

    added pipeline

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
+# from flask import Flask
+# from urllib.parse import quote
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Thank you for checking my repo TY!!!'
+
 from flask import Flask
-from urllib.parse import quote
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Thank you for checking my repo TY!!!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
