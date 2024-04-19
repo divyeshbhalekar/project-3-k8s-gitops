b0397b2b5196ac5e34124fb1d57856b21bc110a7
Divyesh Bhalekar <89533742+divyeshbhalekar@users.noreply.github.com>
2024-04-19 11:28:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, welcome to my page first change"
+    return "Hello, welcome to my page"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
